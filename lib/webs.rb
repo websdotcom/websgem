0f30b5fa71504fb918635f8fc4b8115cd6372afb
chuck <chuck@webs.com>
2010-07-20 16:49:27 -0400

    not using autoload

--- lib/webs.rb
+++ lib/webs.rb
@@ -3,8 +3,6 @@ dir = Pathname(__FILE__).dirname.expand_path
 module Webs
   VERSION = "0.1.0".freeze
 
-  module Controllers
-    autoload :Helpers, 'controller/params'
-  end
-
 end
+
+require dir + 'controller/params'
