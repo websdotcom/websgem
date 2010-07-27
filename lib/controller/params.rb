0f30cf58a827efccf8bb67c6fedced4c4d7208a7
chuck <chuck@webs.com>
2010-07-26 21:03:23 -0400

    added permission methods

--- lib/controller/params.rb
+++ lib/controller/params.rb
@@ -54,11 +54,57 @@ module Webs
       end
       
       # Some basic useful methods
+      def webs_admin_mode?
+        fw_sig_is_admin == '1'
+      end
+      
+      def webs_permission
+        case fw_sig_permissions
+          when /admin/i
+            ADMIN
+          when /owner/i
+            OWNER
+          when /moderator/i
+            MODERATORS
+          when /contributor/i
+            MEMBERS
+          when /limited/i
+            LIMITED
+          else
+            ANYONE
+        end
+      end
+      
+      # does fw_sig_permissions contain at least perm
+      def webs_permission?( perm=ANYONE )
+        webs_permission >= perm
+      end
+
+      def webs_admin?
+        fw_sig_permissions && webs_permission == ADMIN
+      end
+      
+      def webs_owner?
+        fw_sig_permissions && webs_permission == MODERATORS
+      end
+      
+      def webs_contributor?
+        fw_sig_permissions && webs_permission == LIMITED
+      end
+      
+      def webs_moderator?
+        fw_sig_permissions && webs_permission == ANYONE
+      end
+      
+      def webs_site_owner_or_admin?
+        webs_admin? || webs_owner?
+      end
+      
       
       # The full url of the app.  Since in APP_NAME can be different in different environments this is currently
       # defined in a global var, usually in the respective env file, however APP_NAME should be moved to something
       # more elegant in the future.
-      def fw_app_url
+      def webs_app_url
         raise "fw_app_url requires that the constant APP_NAME is defined.. for now..." if !defined?(APP_NAME)
         "#{fw_sig_url}apps/#{APP_NAME}"
       end
--- lib/webs.rb
+++ lib/webs.rb
@@ -3,6 +3,14 @@ dir = Pathname(__FILE__).dirname.expand_path
 module Webs
   VERSION = "0.1.0".freeze
 
+  ANYONE = 0
+  LIMITED = 1
+  MEMBERS = 2
+  MODERATORS = 3
+  OWNER = 4
+  ADMIN = 5
+
+  
 end
 
 require dir + 'controller/params'
