d1fa03d1a45afcecad92544e9d83b420d477971e
Chuck <chuck@snookie.(none)>
2011-02-09 15:57:49 -0500

    ok lets try this!

--- lib/controller/alive_controller.rb
+++ lib/controller/alive_controller.rb
@@ -1,3 +1,5 @@
+require 'socket'
+
 class Webs::AliveController < ActionController::Base
   def index
     # Returns a 500 before ever getting here if cannot connect to database
@@ -8,34 +10,26 @@ class Webs::AliveController < ActionController::Base
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
 
     # Only force down if calling from localhost
-    render( :text=>'OK' ) and return if !request.local?
-    if Webs.cache
-      require 'socket'
-      this_host = Socket.gethostname
-      ENV.keys.sort.each{ |k| Rails.logger.debug "ENV #{k} = #{ENV[k]}"}
-      request.headers.keys.sort.each{ |k| Rails.logger.debug "HEADER #{k} = #{request.headers[k]}"}
-      key = "alive_#{this_host}"
-      Rails.logger.debug "********** Checking Cache for Server Down: #{key}"
-      server_down = Webs.cache.read(key)
-      if server_down.nil? && ['1', '-1'].include?( params['forceDown'])
+    render( :text=>'OK' ) and return unless Webs.cache
+
+    this_host = Socket.gethostname
+    key = "alive_#{this_host}"
+
+    server_down = Webs.cache.read(key)
+    if request.local?
+      if ['1', '-1'].include?( params['forceDown'] )
         server_down = '1'
         Webs.cache.write(key, server_down)
-      elsif server_down && params['forceDown'] == '0'
+      elsif params['forceDown'] == '0'
         Webs.cache.delete(key)
         server_down = nil
       end
-      Rails.logger.debug "********** Server Down?: #{server_down}"
-      if server_down
-        render :text=>"Server is going down!", :status=>503 
-      else
-        render :text=>'OK'
-      end
+    end
+    
+    if server_down
+      render :text=>"Server is going down!", :status=>503 
     else
-      if ['1', '-1'].include?( params['forceDown']) 
-        render :text=>"Server is going down!", :status=>503 
-      else
-        render :text=>'OK'
-      end
+      render :text=>'OK'
     end
   end
 end
