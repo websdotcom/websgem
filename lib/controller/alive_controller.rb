5d4cbdec8e7a527cb255bea16e4a5e1bc59d4c04
chuck <chuck@webs.com>
2011-02-11 18:34:11 -0500

    cache changes to alive controller

--- lib/controller/alive_controller.rb
+++ lib/controller/alive_controller.rb
@@ -12,16 +12,13 @@ class Webs::AliveController < ActionController::Base
     # Only force down if calling from localhost
     render( :text=>'OK' ) and return unless Webs.cache
 
-    this_host = Socket.gethostname
-    key = "alive_#{this_host}"
-
-    server_down = Webs.cache.read(key)
+    server_down = fetch_server_down
     if request.local?
       if ['1', '-1'].include?( params['forceDown'] )
         server_down = '1'
-        Webs.cache.write(key, server_down)
+        set_server_down( server_down )
       elsif params['forceDown'] == '0'
-        Webs.cache.delete(key)
+        Webs.cache.delete(cache_key)
         server_down = nil
       end
     end
@@ -30,6 +27,28 @@ class Webs::AliveController < ActionController::Base
       render :text=>"Server is going down!", :status=>503 
     else
       render :text=>'OK'
+    end
+  end
+
+  private
+  def cache_key
+    this_host = Socket.gethostname
+    "alive_#{this_host}"
+  end
+  
+  def fetch_server_down
+    if Webs.cache.respond_to?( 'read' )
+      Webs.cache.read( cache_key )
+    else
+      Webs.cache[ cache_key ]
+    end
+  end
+  
+  def set_server_down server_down
+    if Webs.cache.respond_to?( 'write' )
+      Webs.cache.write(cache_key, server_down)
+    else
+      Webs.cache[ cache_key ] = server_down
     end
   end
 end
